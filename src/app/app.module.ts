a1bbb85d9364ef8f880ee65e5e71f95a486cb330
sara <sara.atef001@gmail.com>
2023-01-19 13:53:06 +0200

    initalConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { ProductItemComponent } from './../components/product-item/product-item.component';
+import { ProductsComponent } from './../components/products/products.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FooterComponent } from 'src/components/footer/footer.component';
+import { NavbarComponent } from 'src/components/navabar/navbar.component';
+import { SliderComponent } from 'src/components/slider/slider.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SliderComponent,
+    ProductsComponent,
+    ProductItemComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
